041c8fa0e5b35490398f0658d6c6e32003357a55
Adnan Dzafic <adnan.dzafic21@gmail.com>
2016-11-29 15:46:12 +0100

    add /users with template

--- routes/users.js
+++ routes/users.js
@@ -6,11 +6,28 @@ const db = require("./../bin/mysql");
 router.get('/', function(req, res, next) {
   //res.send('respond with a resource');
   db("users").then((users)=> {
-    res.send(users);
+    res.render("users",{
+      title: "All Users",
+       users
+    });
   })
   .catch((err) =>{
     res.send(err);
   });
+});
+
+router.get('/:id',function(req,res,next){
+  db("users")
+    .where("id",req.params.id)
+    .then((users)=>{
+      res.render("users",{
+        title: "User",
+         users
+      });
+    })
+    .catch((err) =>{
+      res.send(err);
+    });
 });
 
 module.exports = router;
